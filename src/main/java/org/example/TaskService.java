93b2bc3186d7481d441d83ba289e8fc7dc119e8d
Carlos <solrak_27@hotmail.com>
2025-11-19 15:24:57 +0100

    Start with taskService

--- src/main/java/org/example/TaskService.java
+++ src/main/java/org/example/TaskService.java
@@ -3,9 +3,30 @@ package org.example;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class TaskService {
 
     @Autowired
     private TaskRepository taskRepository;
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public Optional<Task> getTaskById(Long id) {
+        return taskRepository.findById(id);
+    }
+
+    public Task saveTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public Task updateTask(Long id, Task taskContent) {
+        Task task = taskRepository.findById(id).orElseThrow(() -> new RuntimeException("Task not found"));
+        //updateTask
+
+    }
 }
