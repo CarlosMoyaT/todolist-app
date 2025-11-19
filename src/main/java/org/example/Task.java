5ced6ac8c9335feed5fca3e39c2ff75afd11ed22
Carlos <solrak_27@hotmail.com>
2025-11-19 15:17:52 +0100

    Build CRUD with repository class, service class for clean architecture and good practices

--- src/main/java/org/example/Task.java
+++ src/main/java/org/example/Task.java
@@ -1,15 +1,23 @@
 package org.example;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import java.time.LocalDateTime;
 
+@Entity
 public class Task {
 
-    private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String nameTask;
     private String contentTask;
     private LocalDateTime creationDate;
 
-    public Task(String nameTask, String contentTask, LocalDateTime creationDate, int id) {
+    public Task(String nameTask, String contentTask, LocalDateTime creationDate, Long id) {
         this.nameTask = nameTask;
         this.contentTask = contentTask;
         this.creationDate = LocalDateTime.now();
