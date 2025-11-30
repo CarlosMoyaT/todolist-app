bbbf21d6d7bea63c07491e44310298476ac4c25f
Carlos <solrak_27@hotmail.com>
2025-12-01 00:16:38 +0100

    Entity Task improve it

--- src/main/java/org/example/Task.java
+++ src/main/java/org/example/Task.java
@@ -1,27 +1,46 @@
 package org.example;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Table(name = "tasks")
 public class Task {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, length = 100)
     private String nameTask;
+
+    @Column(length = 500)
     private String contentTask;
+
+    @Column(nullable = false, updatable = false)
     private LocalDateTime creationDate;
 
-    public Task(String nameTask, String contentTask, LocalDateTime creationDate, Long id) {
+    @Column
+    private LocalDateTime updateDate;
+
+    @Column
+    private boolean completed;
+
+    public Task() {
+
+    }
+
+    public Task(String nameTask, String contentTask) {
         this.nameTask = nameTask;
         this.contentTask = contentTask;
         this.creationDate = LocalDateTime.now();
-        this.id = id;
+        this.completed = false;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updateDate = LocalDateTime.now();
     }
 
     public String getNameTask() {
@@ -48,11 +67,31 @@ public class Task {
         this.creationDate = creationDate;
     }
 
-    public int getId() {
-        return id;
+    public LocalDateTime getUpdateDate() {
+        return updateDate;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setUpdateDate(LocalDateTime updateDate) {
+        this.updateDate = updateDate;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", nameTask='" + nameTask + '\'' +
+                ", contentTask='" + contentTask + '\'' +
+                ", creationDate=" + creationDate +
+                ", updateDate=" + updateDate +
+                ", completed=" + completed +
+                '}';
     }
 }
